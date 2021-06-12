464039e32d8e7e209e1e5e90a8daee2564818166
Matt Shore <mazzamds@gmail.com>
2021-06-12 01:04:05 +0100

    Front end update.

--- Web Application/wwwroot/js/site.js
+++ Web Application/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function timeline() {
+    let dates = document.querySelectorAll('.col-sm');
+    for (let i = 0; i < dates.length; i++) {
+        dates[i].addEventListener('mouseover', function () {
+            console.log('hello');
+        });
+    }
+
+        
+}
+
+try {
+    if (window.location.href.includes('/Timeline')) {
+        timeline();
+    }
+} catch (e) {
+    console.log(e);
+}
